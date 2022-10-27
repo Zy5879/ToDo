2a73f9df9672dc8feb7e8c6cc021e677e52583e5
Zy5879 <ZaireM21@gmail.com>
2022-10-26 22:22:54 -0400

    foundation of project

---
+++ dist/bundle5a1506a84aa73253bf15.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+const h1 = document.createElement('h1')
+h1.textContent = 'HELLO'
+document.body.appendChild(h1)
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlNWExNTA2YTg0YWE3MzI1M2JmMTUuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBQTtBQUNBO0FBQ0EsNkIiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly90b2RvLy4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnN0IGgxID0gZG9jdW1lbnQuY3JlYXRlRWxlbWVudCgnaDEnKVxuaDEudGV4dENvbnRlbnQgPSAnSEVMTE8nXG5kb2N1bWVudC5ib2R5LmFwcGVuZENoaWxkKGgxKSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
