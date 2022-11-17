0b503c99e899b977c43a0b52b05b9c03850cd211
Zy5879 <ZaireM21@gmail.com>
2022-11-17 15:40:09 -0500

    added a pubsub method

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import {ProjectName}  from "./modules/project.js";
-import  {Todo}  from "./modules/todolist.js";
-import {User}  from "./modules/user.js";
-import { UI } from "./modules/display.js";
-import './styles/style.css'
