bb5ee93b952dc07b9199fc37ed185c4da01dc70c
Zy5879 <ZaireM21@gmail.com>
2022-12-06 20:29:05 -0500

    change task button pops up on click

--- src/modules/task.js
+++ src/modules/task.js
@@ -43,22 +43,74 @@ export const todo = (() => {
         // let divul = document.createElement('ul')
         // div.appendChild(divul)
         let divbutton = document.createElement('button')
+        divbutton.classList.add('task-content')
+        let btnright = document.createElement('div')
+        btnright.classList.add('right-area')
+        let btnp = document.createElement('p')
+
+        btnp.classList.add('task-name')
+        // })
+
+        // btnp.addEventListener('click', (e) => {
+        //         const taskname = document.querySelector('.task-name')
+        //         taskname.style.display = 'none';
+        
+        //         const inputtask = document.querySelector('.change-task')
+        //         inputtask.style.display = 'block';
+                
+        //         e.preventDefault()
+    
+        // })
+
+        // let btninput = document.createElement('input')
+        // btninput.setAttribute('type','text')
+        // btninput.setAttribute('placeholder', `${todo}`)
+        // btninput.classList.add('change-task')
+
+
+        btnright.append(btnp,)
+        divbutton.appendChild(btnright)
         div.appendChild(divbutton)
 
-        divbutton.innerText = ''
+        btnp.innerText = ''
         // let df = document.createDocumentFragment()
         tasklist.forEach(todos => {
-            divbutton.innerText = `${todos.task}`
+            btnp.innerText = `${todos.task}`
+            // btnp.addEventListener('click', changeTask)
+            // btnp.addEventListener('click', () => {
+            //     console.log(`${todos.task}`)
+            // })
             // let li = document.createElement('li')
             // li.classList.add('task-name')
             // li.innerText = `${todos.task}`;
             // df.appendChild(li)
         })
 
+        // function changeTask() {
+            
+        // }
+
+        
+
         // divul.appendChild(df)
 
         const holdtask = document.querySelector('.holdtask')
         holdtask.appendChild(div)
+
+        console.log(document.querySelectorAll('.task-name'))
+
+        // const btn = document.querySelectorAll('.task-name')
+
+
+        document.querySelectorAll('.task-name').forEach(btn => {
+            btn.addEventListener('click',() => {
+                btn.style.display  = 'none';
+                const input = document.createElement('input')
+                input.setAttribute('type','text')
+                btnright.appendChild(input)
+            })
+        })
+
 
         // let ul = document.querySelector('.todotask ul')
         // ul.innerText = ''
@@ -73,6 +125,91 @@ export const todo = (() => {
         // ul.appendChild(df)
 
     }
+
+    function changeTask() {
+       
+    }
+
+    // document.querySelectorAll('.task-name').forEach(btn => {
+    //     btn.addEventListener('click', changeTask)
+    // })
+    // function changeTask() {
+        // document.addEventListener('DOMContentLoaded', () => {
+        //     document.querySelectorAll('.task-name').forEach((btn) => {
+        //         btn.onclick = () => changeTask
+
+        //     })
+        // })
+
+        // document.querySelectorAll('.task-name').forEach(btn => {
+        //     btn.addEventListener('click', () => {
+        //         console.log('Hey')
+        //     })
+        // })
+    //    const input = document.querySelectorAll('.change-task')
+    //    input.forEach(inp => {
+    //    inp.style.display = 'block'
+    //    const btn = document.querySelector('.task-name')
+    //    btn.style.display = 'none'
+    //    })
+
+    // const btn = document.querySelectorAll('.task-name')
+    // btn.forEach(button => {
+    //     button.style.display = 'none'
+    //     const input = document.querySelectorAll('.change-task')
+    //     input.forEach(inp => {
+    //         inp.style.display = 'block'
+    //     })
+    // })
+
+    // }
+
+    // function changeTask() {
+    //     const input = document.querySelectorAll('.change-task')
+    //     input.forEach(inp => {
+    //     inp.style.display = 'block'
+    //     })
+    // }
+
+
+    // const btn = document.querySelectorAll('.task-name')
+    // btn.forEach((button, index) => {
+    //     button.addEventListener('click',() => {
+    //         console.log(index)
+    //     })
+    // })
+
+
+    // function changeTask() {
+        // e.preventDefault()
+
+        // const btn = document.querySelectorAll('.task-name')
+        // btn.forEach(function(button) {
+        //     button.addEventListener('click', function() {
+        //         console.log(`${list.task}`)
+
+        //     })
+        // })
+        // btn.addEventListener('click', (e) => {
+        //     const taskname = document.querySelector('.task-name')
+        //     taskname.style.display = 'none';
+    
+        //     const inputtask = document.querySelector('.change-task')
+        //     inputtask.style.display = 'block';
+            
+        //     e.preventDefault(e)
+
+        // })
+    // }
+
+        // const taskname = document.querySelector('.task-name')
+        // taskname.style.display = 'none';
+
+        // const inputtask = document.querySelector('.change-task')
+        // inputtask.style.display = 'block';
+
+    // }
+
 
     return {
         addTask,
